76a06c14c023f9d8453a5ed891d761f9a339f967
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-05-17 14:56:50 +0200

    convention code

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -5,18 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject player;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         Destroy(player);
