446486b86cf71c2e835a1affde365b9172c6824c
AngelSushi <angelsushigame@gmail.com>
2023-06-02 10:26:09 +0200

    Add Limit OneWay

--- Assets/Scripts/Plateform.cs
+++ Assets/Scripts/Plateform.cs
@@ -9,6 +9,8 @@ public class Plateform : MonoBehaviour
     private BoxCollider2D _collider2D;
     private Vector2 _enterDashDirection;
 
+    public float downMin;
+
     private void Start()
     {
         _collider2D = GetComponent<BoxCollider2D>();
@@ -18,14 +20,8 @@ public class Plateform : MonoBehaviour
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-
             PlayerDash playerDash = col.gameObject.GetComponent<PlayerDash>();
-            Debug.Log("stay ");
-           // Debug.Log("ignore collision " +( playerDash.dashDirection.y < 0) + "dashDirection " + playerDash.dashDirection);
-            Physics2D.IgnoreCollision(_collider2D,col.collider,playerDash.dashDirection.y < 0);
-            
-            
-            
+            Physics2D.IgnoreCollision(_collider2D,col.collider,playerDash.dashDirection.y < downMin);
         }
     }
     
@@ -33,7 +29,6 @@ public class Plateform : MonoBehaviour
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            Debug.Log("exit");
             StartCoroutine(EnableCollision(col));
         }
         
@@ -43,6 +38,5 @@ public class Plateform : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         Physics2D.IgnoreCollision(_collider2D,col.collider,false);
-        Debug.Log("step to false");
     }
 }
