ee62b78445f8a8e61805997a94513fb29b9313ba
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-06-08 16:08:42 +0200

    [add]animation landing dash etc

--- Assets/Scripts/Gameplay/PlayerDash.cs
+++ Assets/Scripts/Gameplay/PlayerDash.cs
@@ -30,6 +30,10 @@ public class PlayerDash : MonoBehaviour {
     [SerializeField] private Transform distanceCombo;
     private float comboPoint = 0;
 
+    [Header("player")]
+    public GameObject groundDetection;
+    public GameObject landingAnimation;
+    public Animator playerAnimator;
     
     [Header("Debug")]
     [SerializeField] private bool debugDash;
@@ -52,8 +56,13 @@ public class PlayerDash : MonoBehaviour {
         _rb = GetComponent<Rigidbody2D>();
     }
     
-    void Update() {
-        if (Input.touchCount > 0) {
+    void Update() 
+    {
+
+        playerAnimator.SetBool("Dash", isDashing);
+
+        if (Input.touchCount > 0) 
+        {
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
@@ -77,7 +86,7 @@ public class PlayerDash : MonoBehaviour {
                             {
                                 dashDirection = (_endPosition - _startPosition).normalized;
                                 _rb.velocity = dashDirection * dashSpeed;
-
+                                //ici
                                 _playerPosition = transform.position;
                                 isDashing = true;
                             }
@@ -94,6 +103,7 @@ public class PlayerDash : MonoBehaviour {
                     {
                         dashDirection = (_endPosition - _startPosition).normalized;
                         _rb.velocity = dashDirection * dashSpeed;
+                        //ici
 
                         _playerPosition = transform.position;
                         isDashing = true;
@@ -108,6 +118,8 @@ public class PlayerDash : MonoBehaviour {
             _rb.velocity = Vector2.zero;
             dashDirection = Vector2.zero;
             isDashing = false;
+            //ici la 
+
         }
     }
 
@@ -122,6 +134,11 @@ public class PlayerDash : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        if(col.tag == "Ground")
+        {
+            Debug.Log("Le player touche le sol wesh");
+            StartCoroutine(StartAndDestroyAnim());
+        }
         if (col.gameObject.layer == LayerMask.NameToLayer("Destructible") && isDashing)
         {
             if (debugDash)
@@ -241,6 +258,12 @@ public class PlayerDash : MonoBehaviour {
             }
         }
     }
+    public IEnumerator StartAndDestroyAnim()
+    {
+        var myNewSmoke = Instantiate(landingAnimation, groundDetection.transform.position, Quaternion.identity);
+        myNewSmoke.transform.parent = gameObject.transform;
+        yield return new WaitForSeconds(0.389f);
+    }
     private void ResetDash()
     {
         isDashing = false;
@@ -267,4 +290,9 @@ public class PlayerDash : MonoBehaviour {
         Debug.Log("le combot point est de " + comboPoint);
 
     }
+    //private bool IsGrounded()
+    //{
+    //    return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded;
+    //}
+    
 }
